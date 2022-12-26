d41ae1e75ce1ef35af9f61038f14cc71cd00c2bb
hamza97benroummane <hamza.benroummane@gmail.com>
2022-12-26 14:26:15 +0100

    commit 2

--- src/main/java/com/example/fournisseurs/resources/FrResource.java
+++ src/main/java/com/example/fournisseurs/resources/FrResource.java
@@ -15,7 +15,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @RequestScoped
-@Path("todos")
+@Path("fournisseur")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class FrResource {
